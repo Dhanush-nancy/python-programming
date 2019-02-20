0983a0c1fb837146daeba097dde075869aadfedd
Dhanush-nancy <42996626+Dhanush-nancy@users.noreply.github.com>
2019-02-20 14:39:17 +0530

    Create append.c

---
+++ append.c
@@ -0,0 +1,8 @@
+first_name = raw_input("Enter your first name: ")
+last_name = raw_input("Enter your last name: ")
+print "Enter your date of birth" 
+month= raw_input("Month? ")
+day= raw_input("Day? ")
+year= raw_input("Year? ")
+print "Here's your information"
+print first_name, "was born on", month, day+',', year.
